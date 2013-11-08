6f097457a968b0a3621358f0ebc2c5e3a7126ca8
tonghuashuai <tonghuashuai@126.com>
2013-11-08 13:06:30 +0800

    增加召唤模式

--- static/js/admin.js
+++ static/js/admin.js
@@ -2,16 +2,48 @@
  * Created by Administrator on 13-11-7.
  */
 $(document).ready(function(){
-    $('#more').click(showOrHide);
-    $('#allowtab').allowtab();
+    //单击其他地方隐藏菜单
+    $(document).click(function(e){
+        if (e.target != null && e.target.id != 'other_more'){
+            if ($('#other_new').css('display') != 'none'){
+                hideOtherMore();
+            }
+        } else {
+            showOrHide();
+        }
+
+    });
+
+    //文章内容textarea允许tab键
+    $('#txt_content').allowtab();
+
+    //标题和内容召唤模式，使用@召唤分类和标签
+    $('#txt_title').atwho({
+        at: "@cat:",
+        data: ["+1", "-1", "s"]
+    });
+
+    $('#txt_content').atwho({
+        at: "@tag:",
+        data: ["+1", "-1", "smile"]
+    });
+
 });
 
 function showOrHide(){
-    if ($('#new').css('display') == 'none'){
-        $('#new').css('display', 'block');
-        $('#more').css('background-color', '#ab3720');
+    if ($('#other_new').css('display') == 'none'){
+        showOtherMore();
     } else {
-        $('#new').css('display', 'none');
-        $('#more').css('background-color', '#d84c31');
+        hideOtherMore();
     }
+}
+
+function showOtherMore(){
+    $('#other_new').css('display', 'block');
+    $('#other_more').css('background-color', '#ab3720');
+}
+
+function hideOtherMore(){
+    $('#other_new').css('display', 'none');
+    $('#other_more').css('background-color', '#d84c31');
 }
