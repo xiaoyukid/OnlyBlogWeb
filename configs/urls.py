7a61cbcef3c5e42df1d38f308723e9a0b4268e1a
tonghuashuai <tonghuashuai@126.com>
2013-11-14 13:39:45 +0800

    uWSGI autoreload test

--- configs/urls.py
+++ configs/urls.py
@@ -6,7 +6,7 @@ __author__ = 'Administrator'
 urls = (
     '/favicon.ico', faviconICO,
 
-    '/', install,
+    '/', index,
     '/page/(\d+)/(\d+)/', index,
     '/page/(\d+)/(\d+)', index,
     '/page/(\d+)/', index,
